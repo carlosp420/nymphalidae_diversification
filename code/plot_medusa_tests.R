509f9d08b534c97a1959c7b6bf7e0e31dd29a83f
Carlos.Peña <mycalesis@gmail.com>
2013-08-10 12:27:30 +0300

    adding code to plot effect of confidence intervals on multiMEDUSA

--- code/plot_medusa_tests.R
+++ code/plot_medusa_tests.R
@@ -34,3 +34,18 @@ p <- qplot(data$V1, data$V2, geom="point",  main="Effect of percentage of
             multiMEDUSA analyses")
 p + geom_smooth(method="lm", se=FALSE, aes(data$V1)) + theme_bw()
 dev.off()
+
+
+## plot width of confidence intervals for estimated ages versus consistently recovered nodes
+multimedusa <- read.csv("output/multimedusa_output.csv", header=FALSE)
+par(new=TRUE)
+png(filename="figures/plot_multimedusa_tests_on_confidence_intervals.png")
+p <- qplot(multimedusa$V1, multimedusa$V2, geom="line",  size=I(1),
+            main="Effect of confidence interval width of estimated ages
+            on multiMEDUSA results",
+            xlab="sets of trees with decreasing width of confidence intervals",
+            ylab="number of nodes consistenly recovered from the
+            multiMEDUSA analyses")
+p + scale_y_continuous(breaks=c(5, 7.5, 10, 12.5, 15), 
+                       labels=c("5", "7.5", "10", "12.5", "15")) + theme_bw()
+dev.off()
