5b4a67222eae023339133938a181c9899232b547
Carlos Pena <mycalesis@gmail.com>
2013-08-19 16:01:51 +0300

    fix makin figure2

--- code/make_figure_02_medusa.R
+++ code/make_figure_02_medusa.R
@@ -9,9 +9,10 @@ load("ancillary/supp_mat_10_MEDUSA_Nymphalidae_raw_data.csv")
 
 devSVG(file=as.character(args[1]), width=9, height=19)
 summarize.turboMEDUSA(res, cex=0.3)
-legend("left", legend=c("6. Ithomiini", "4. Charaxes", "9. Danaina"),
+legend(20,20, legend=c("6. Ithomiini", "4. Charaxes", "9. Danaina"),
                 title="Diversification shifts inferred in more \n than 95% of the posterior distribution of trees",
                 xjust=0.5,
                 bty="o",
-                xpd=TRUE)
+                xpd=TRUE,
+                cex=1.2)
 dev.off()
