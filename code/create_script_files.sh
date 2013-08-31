f905404f51c1fcc3e18dda2c24a489a3a574cf23
Carlos Peña <mycalesis@gmail.com>
2013-08-31 13:42:53 +0300

    small change for script to run in CSC

--- code/create_script_files.sh
+++ code/create_script_files.sh
@@ -2,5 +2,7 @@ for i in {1..1000}
     do
         cp code/script_for_multimedusa_in_cluster.sh "script_$i.sh"
         sed -i -r "s/set_8/set_$i/g" "script_$i.sh"
+        sed -i "s/output/output\/variable_topology/g" "script_$i.sh"
+        sed -i "s/multi_$/multi_$i/g" "script_$i.sh"
         echo "$i"
     done
