68a4341f38dee10012db0f19fb7158d80b6d220c
Carlos Peña <mycalesis@gmail.com>
2014-05-26 11:49:20 +0300

    included alt tree with nodes and probs to manuscript

--- ancillary/supp_mat_06_run_multimedusa.R
+++ ancillary/supp_mat_06_run_multimedusa.R
@@ -9,9 +9,13 @@ richness <- read.csv("ancillary/supp_mat_03_richness.csv")
 res <- MEDUSA(phy, richness, stop="threshold", model="mixed", modelLimit=27, mc=TRUE)
 
 
-pdf(file="ancillary/supp_mat_08_multimedusa_result01.pdf", width=9, height=19)
+pdf(file="ancillary/supp_mat_08_multimedusa_result01.pdf", width=9, height=22)
 summ <- multiMedusaSummary(res, conTree=mct, cex=0.3)
 dev.off()
 
+source("code/alt_plotMultimedusa.R")
+pdf(file="ancillary/supp_mat_14_multimedusa_result_rates_nodes.pdf", width=9, height=22)
+plotMultiMedusa_alt(summ)
+dev.off()
 
 save(res, file="ancillary/supp_mat_07_multimedusa_on_1000_trees.txt", ascii=TRUE)
