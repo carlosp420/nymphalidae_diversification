3dd21765a7e6cb5a89900bc6f4f216240ba15229
Carlos Peña <mycalesis@gmail.com>
2014-06-16 17:46:46 +0300

    some supp mat

--- ancillary/supp_mat_06_run_multimedusa.R
+++ ancillary/supp_mat_06_run_multimedusa.R
@@ -38,3 +38,50 @@ p + geom_errorbar(aes(ymax=max.shift, ymin=min.shift), position=dodge, width=2) 
   theme(axis.text.x = element_text(angle=45, hjust=1, size=10))
 
 
+# find how many times a node is present in the 1000 trees
+library("phytools")
+
+# for a given tree, get its descendant tip names for a given node
+mct <- read.nexus("ancillary/supp_mat_09_mcc_tree_from_random_sample_of_1000.tree")
+phy <- read.nexus("ancillary/supp_mat_05_1000_random_trees_no_outgroups.nex")
+
+get_descendant_tips <- function(phy, node) {
+  tips <- c()
+  desc <- getDescendants(phy, node)
+  for( j in which(desc < 399) ) {
+    tips <- c(tips, desc[j])
+  }
+  names <- c()
+  for( i in tips ) {
+    names <- c(names, phy$tip.label[i])
+  }
+  sort(names)
+}
+
+oldest.mrca<-function(tree,tips){
+  H<-nodeHeights(tree)
+  X<-mrca(tree)
+  n<-length(tips)
+  nodes<-height<-vector(); k<-1
+  for(i in 1:(n-1)) for(j in (i+1):n){
+    nodes[k]<-X[tips[i],tips[j]]
+    height[k]<-H[match(nodes[k],tree$edge[,1]),1]
+    k<-k+1
+  }
+  z<-match(min(height),height)
+  return(nodes[z])
+}
+
+
+# node 458 ->  0.0 in all 1000 trees
+# node 625
+
+expected_tips <- get_descendant_tips(mct, 625)
+results <- c()
+for (i in 1:length(phy)) {
+  this.mrca <- oldest.mrca(phy[[i]], expected_tips)
+  tips <- get_descendant_tips(phy[[i]], this.mrca)
+  
+  # if the descendants are the same, the lenght of the difference between lists should be 0
+  results <- c(results, setequal(sort(expected_tips), sort(tips)))
+}
