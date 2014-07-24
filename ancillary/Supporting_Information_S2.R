426b10dfcf1e8ffa184e9b65ed2d4a34f5b43c65
Carlos Peña <mycalesis@gmail.com>
2014-07-24 11:25:57 +0300

    ready to submit

---
+++ ancillary/Supporting_Information_S2.R
@@ -0,0 +1,24 @@
+# run MEDUSA 0.93-4-33
+
+library("MEDUSA")
+library("multicore")
+phy <- read.nexus("ancillary/supp_mat_01_genus.nex");
+phy <- ladderize(phy)
+
+# we have to remove the tips
+tips <- c("Achlyodes", "Graphium", "Parnassius", "Baronia", "Troides", "Papilio1", "Papilio2", "Pieris", "Aporia", "Styx", "Hamearis", "Euselasia", "Nymphidium", "Emesis", "Crocozona", "Riodina", "Amarynthis", "Baliochila", "Poritia", "Miletus", "Liphyra", "Lycaena", "Celastrina", "Thecla", "Lucia", "Curetis", "Eurema", "Colias", "Leptidea", "Pseudopontia", "Libyt")
+phy <- drop.tip(phy, tip=tips)
+
+
+richness <- read.csv("ancillary/supp_mat_03_richness.csv")
+
+res <- MEDUSA(phy, richness, stop="threshold", model="mixed", modelLimit=25, mc=TRUE)
+
+
+pdf(file="output/medusa_on_mct.pdf", width=9, height=19)
+summ <- medusaSummary(res, cex=0.3)
+dev.off()
+
+
+save(res, file="ancillary/supp_mat_04_MEDUSA_results.txt", ascii=TRUE)
+
